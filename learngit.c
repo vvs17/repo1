827368c119f32d340215aaba7f079af0ae7db766
Victor Vianna <victorvsantos@Niffheim.local>
2018-04-10 16:15:40 -0300

    1o commit

---
+++ learngit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int soma(int x,int y);
+int media(int x, int y);
+
+int main() {
+    int x = 2;
+    int y = 4;
+
+    int res1 = soma(x,y);
+    int res2 = media(x,y);
+}
+
+int soma(int x, int y) {
+    return x + y;
+}
+
+int media(int x, int y) {
+    return ((x + y) / 2);
+}
